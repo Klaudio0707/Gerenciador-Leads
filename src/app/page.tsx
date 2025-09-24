e57c56fee39505f437874c4a8dcd8521d535a7d1
*Bowie* <claudioroberto0707@outlook.com>
2025-09-24 11:00:38 -0300

    style: css do home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 import styles from "./home.module.css";
-
+import { IoCreateSharp } from "react-icons/io5";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
 export default function Home() {
   return (
     <main className={styles.container}>
      <h2 className={styles.title}>Gerenciador de Leads</h2>
      <p className={styles.subtitle}>Bem-vindo ao Gerenciador de Leads</p>
-      <div>
+      <div className={styles.cardContainer}>
         <Link href="/leads" className={styles.card}>
           <h2>Visualizar Leads</h2>
           <p>Ver e gerenciar a lista de leads cadastrados.</p>
+          <MdOutlineDashboardCustomize size={40} />
         </Link>
         <Link href="/cadastro" className={styles.card}>
           <h2>Cadastrar Lead</h2>
           <p>Adicionar um novo lead ao sistema.</p>
+          <IoCreateSharp  size={40}/>
         </Link>
       </div>
     </main>
