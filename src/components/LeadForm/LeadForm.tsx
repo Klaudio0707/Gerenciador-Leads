b06812ef27a75762a210712c8357b2e0fd15ecf7
*Bowie* <claudioroberto0707@outlook.com>
2025-09-24 15:17:00 -0300

    style: estilização do component leadForm

--- src/components/LeadForm/LeadForm.tsx
+++ src/components/LeadForm/LeadForm.tsx
@@ -31,42 +31,41 @@ export function LeadForm() {
 
                 setTimeout(() => {
                     window.open(whatsappUrl, '_blank');
-                }, 2000);
-
-            }else{
+                }, 2000); // pausa para leitura de mensagem
+               
+            } else {
                 const errorData = await response.json();
                 setMessage(`Erro: ${errorData.message || 'Não foi possível realizar o cadastro.'}`);
             }
         } catch (error) {
             setMessage(`Erro: ${error || 'Não foi possível realizar o cadastro.'}. Tente novamente mais tarde.`);
-        }finally {
+        } finally {
             setIsLoading(false);
         }
-        };
+    };
 
     return (
-            <main className={styles.container}>
-                <h1 className={styles.title}>Cadastro de Leads</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className={styles.formGroup}>
-                        <label htmlFor="nome">Nome</label>
-                        <input id="nome" type="text" value={nome} placeholder='Nome' onChange={(e) => setNome(e.target.value)} required />
-                    </div>
-                    <div className={styles.formGroup}>
-                        <label htmlFor="email">E-mail</label>
-                        <input id="email" type="email" value={email} placeholder='email@email.com' onChange={(e) => setEmail(e.target.value)} required />
-                    </div>
-                    <div className={styles.formGroup}>
-                        <label htmlFor="telefone">Telefone</label>
+        <main className={styles.container}>
+            <h1 className={styles.title}>Cadastro de Leads</h1>
+            <form onSubmit={handleSubmit}>
+                <div className={styles.formGroup}>
+                    <label htmlFor="nome">Nome</label>
+                    <input id="nome" type="text" value={nome} placeholder='Nome' onChange={(e) => setNome(e.target.value)} required />
+                </div>
+                <div className={styles.formGroup}>
+                    <label htmlFor="email">E-mail</label>
+                    <input id="email" type="email" value={email} placeholder='email@email.com' onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div className={styles.formGroup}>
+                    <label htmlFor="telefone">Telefone</label>
+                    <input id="telefone" type="tel" value={telefone} placeholder="81999998888" onChange={(e) => setTelefone(e.target.value)} required />
+                </div>
+                <button type="submit" disabled={isLoading} className={styles.submitButton}>
+                    {isLoading ? 'Enviando...' : 'Enviar'}
+                </button>
+            </form>
+            {message && <p className={styles.message}>{message}</p>}
+        </main >
+    );
 
-                        <input id="telefone" type="tel" value={telefone} placeholder="81999998888" onChange={(e) => setTelefone(e.target.value)} required />
-                    </div>
-                    <button type="submit" disabled={isLoading} className={styles.submitButton}>
-                        {isLoading ? 'Enviando...' : 'Enviar'}
-                    </button>
-                </form>
-                {message && <p className={styles.message}>{message}</p>}
-            </main >
-        );
-
-    }
+}
